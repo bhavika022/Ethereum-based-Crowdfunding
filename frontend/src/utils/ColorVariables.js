b84041e4344f2397bf19d9e8a69c71ef0b21878e
Bhavika Kulkarni <57183119+bhavika022@users.noreply.github.com>
2020-09-04 17:29:24 +0530

    Add files via upload

---
+++ frontend/src/utils/ColorVariables.js
@@ -0,0 +1,12 @@
+const colorVariables = {
+  fontColor1: "#4646BA",
+  fontColor2: "#F9CA5B",
+  //fontColor3: '#f8e9d1',
+  fontColor3: "#3A405F",
+  //darkGold: '#b99458',
+  backgroundColor1: "#FFFFFF",
+  backgroundColor2: "#FAFAFB",
+  backgroundColor3: "#F3F3F3",
+};
+
+export default colorVariables;
